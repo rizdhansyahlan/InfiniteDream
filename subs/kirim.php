d1f6728768b5b2fc53e163c4c63cc24d923900b2
rizdhansyahlan <rizdhan2002@gmail.com>
2024-05-09 16:54:28 +0700

    Add files via upload

---
+++ subs/kirim.php
@@ -0,0 +1,51 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+if(isset($_POST['subscribe']))
+{
+    // Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+    $email              = $_POST['email'];
+
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'infinitedreamcom@gmail.com';                     // SMTP username
+    $mail->Password   = 'yfan pzfd ukli hoce';                               // SMTP password
+    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('infinitedreamcom@gmail.com', 'Subscription');
+    $mail->addAddress($email);     // Add a recipient
+    
+    $mail->addReplyTo('infinitedreamcom@gmail.com', 'Subscription');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    // Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Thanks For Subscribing Us - Infinite Dream';
+    $mail->Body    = file_get_contents("email.html");
+    if($mail->send())
+    {
+        header("Location: /uas_wp7/1_html/a_home.php#input-field");
+        exit; // Ensure that no further code is executed after the redirect
+    }
+    else{
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
