8c18678ad147a4b584fe60350133db77009e0e4a
rizdhansyahlan <rizdhan2002@gmail.com>
2024-05-25 19:36:09 +0700

    p

--- subs/kirim.php
+++ subs/kirim.php
@@ -19,16 +19,16 @@ if(isset($_POST['subscribe']))
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'infinitedreamcom@gmail.com';                     // SMTP username
-    $mail->Password   = 'yfan pzfd ukli hoce';                               // SMTP password
+    $mail->Username   = '';                     // SMTP username
+    $mail->Password   = '';                               // SMTP password
     // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('infinitedreamcom@gmail.com', 'Subscription');
+    $mail->setFrom('', 'Subscription');
     $mail->addAddress($email);     // Add a recipient
     
-    $mail->addReplyTo('infinitedreamcom@gmail.com', 'Subscription');
+    $mail->addReplyTo('', 'Subscription');
     // $mail->addCC('cc@example.com');
     // $mail->addBCC('bcc@example.com');
 
